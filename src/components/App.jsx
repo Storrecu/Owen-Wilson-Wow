b9ac66ddaa813dbc131d2b621fc01c9b82fdd8ea
Silvia <silviatc1993@gmail.com>
2023-10-20 16:45:56 +0200

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,32 @@
+//imports dependencias, images, componentes, styles
+// import { useEffect, useState } from 'react';
+// import callToApi from '../services/api';
+// import ls from '../services/localStorage';
+//import PropTypes from "prop-types";
+// import { Link, Route, Routes } from "react-router-dom";
+//import { useLocation, matchPath } from "react-router";
+//import { useParams } from "react-router-dom";
+import '../styles/App.scss';
+
+function App() {
+  //states
+
+  //effects
+
+  //handlers from events
+
+  //renders
+
+  //html
+  return (
+    <>
+      <p></p>
+    </>
+  );
+}
+//NombreDeMiComponente.propTypes = {
+//   nombreDeMiPropDeTipoStringOpcional: PropTypes.string,
+//   nombreDeMiPropDeTipoStringObligatoria: PropTypes.string.isRequired,
+// };
+
+export default App;
